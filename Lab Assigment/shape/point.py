5144ded54454b15c52fcf887bdbaa057a617a6f3
Shashank Sinha <shashank.sinha@rungta.org>
2026-01-12 18:26:24 +0530

    Add files via upload

---
+++ Lab Assigment/shape/point.py
@@ -0,0 +1,4 @@
+class Point:
+    def _init_(self, x=0, y=0):
+        self.x = x
+        self.y = y
---
+++ Lab Assigment/shapee/point.py
@@ -0,0 +1,4 @@
+class A:
+    def _init_(self, x=0, y=0):
+        self.x = x
+        self.y = y
